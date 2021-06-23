29fe500cc7e1fac81085cc19d14a9a676795e761
liaronce <liaronce@hotmail.com>
2021-06-23 18:41:40 +0800

    Change build script.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e #fail on error
+ls application
+cd application
+zip -r application.zip ./*
+mv application.zip ../application.zip
+cd ..
+zip -r openkaios-store-nightly.zip application.zip metadata.json
+mkdir workdir
+mv application.zip workdir/application.zip
+mv metadata.json workdir/metadata.json
