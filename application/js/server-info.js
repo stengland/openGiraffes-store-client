c25e627cd0e104c300a963c64dd5dbc8c83f250c
liaronce <liaronce@hotmail.com>
2021-07-07 23:52:53 +0800

    Change domain.

--- application/js/server-info.js
+++ application/js/server-info.js
@@ -1,8 +1,8 @@
 const server_info = {
     name: "opengiraffes",
-    LastUpdateFile: "https://opengiraffes.com/opengiraffes-store-db/lastUpdate.txt",
-    server_list: ["https://storedb.opengiraffes.com/data.json"],
+    LastUpdateFile: "https://storedb.opengiraffes.top/lastUpdate.txt",
+    server_list: ["https://storedb.opengiraffes.top/data.json"],
     SimpleRatingServer: "https://liaronce.top",
     QR_header: "opengiraffes:",
-    terms: "https://docs.opengiraffes.com/#/store/terms",
+    terms: "https://docs.opengiraffes.top/#/store/terms",
 };
